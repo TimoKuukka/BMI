6c47f8aef1889f975db1bc95e2489a1dfdbcd198
TimoKuukka <46355@edu.raseko.fi>
2023-01-27 14:40:49 +0200

    Rakenteelliset tyypit

---
+++ kysy_tiedot.py
@@ -0,0 +1,58 @@
+# OHJELMA, JOKA KYSYY BMI-TIETOJA USEASTA KUNTOILIJASTA
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+# KIRJASTOT JA MODUULIT
+#>>>>>>>>>>>>>>>>>>>>>>
+
+# Tuodaan fitness.py:n sisältämät toiminnot tähän ohjelmaan
+import fitness
+
+bmi_lista = []
+nimilista = []
+
+# Kysytään tiedot ja tulostetaan painoindeksi kunnes halutaan lopettaa
+while True: # Ikuinen silmukka
+
+    nimi = input("Nimi, tyhjä lopettaa: ")
+
+    if nimi == "":
+        break
+    
+    nimilista.append(nimi)
+    pituus_teksti = input("Pituus(cm): ")
+    paino_teksti = input("Paino (kg): ")
+
+    # Yritetään muuttaa syötetyt tekstit luvuiksi ja laskea BMI
+    try:
+        pituus = float(pituus_teksti)
+        paino = float(paino_teksti)
+        
+        # Lasketaan painoindeksi fitness-modulin laske_bmi funktiolla
+        bmi = fitness.laske_bmi(pituus, paino)
+
+        # Luodaan monikko (tuple), jossa nimi ja bmi
+        monikko = (nimi, bmi)
+
+        # Lisätään BMI listaan
+        bmi_lista.append(monikko)
+
+        # Näytetään tulokset ruudulla
+        print ("Painoindeksi on", bmi)
+
+    # Jos tapahtuu virhe, ilmoitetaan käyttäjälle
+    except Exception as e:
+        print("Syötteessä oli virhe, yritä uudelleen", e)
+
+print("Nimet ja painoindeksit olivat :", bmi_lista)
+
+# Puretaan lista ja tulostetaan se rivi riviltä -> monikko / rivi
+for henkilo in bmi_lista:
+
+    # Monikossa on kaksi tietoa, joiden indeksit ovat 0 (ensimmäinen) ja 1 (toinen)
+    print(henkilo[0], " painoindeksi on", henkilo[1])
+
+#Listassa olevien monikoiden määrä
+print("Listassa oli,", len(bmi_lista), "merkintää")
+
+nimilista.sort() # Aakkostetaan nimilista
+print(nimilista) # Tulostetaan nimilista
